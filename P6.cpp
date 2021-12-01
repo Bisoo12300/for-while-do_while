e2bfcc0fc7b5f3770c09d5b001f9a460af6c3081
Gabien21 <93122206+Gabien21@users.noreply.github.com>
2021-12-01 21:09:20 +0700

    Update P6.cpp

--- P6.cpp
+++ P6.cpp
@@ -1,3 +1,6 @@
+//VCT hiển thị đồng hồ theo đúng dạng <hh:mm:ss AM/PM> (giờ của đồng hồ được lấy theo giờ của máy), khi chỉ số giây ss=0 thì phát ra 1 tiếng bíp. 
+//Chương trình kết thúc khi người dùng nhấn phím Esc hoặc khi đã chạy đủ số giây được nhập vào trước đó.
+
 #include <iostream>
 #include <ctime>
 #include <conio.h>
